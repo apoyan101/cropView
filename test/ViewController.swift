ee2e92877fdf24183c58e86cff20fcf74d813bb3
Ara Apoyan <ara.apoyan@digitain.com>
2022-08-26 13:11:43 +0400

    Set up project.

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Ara Apoyan on 26.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
